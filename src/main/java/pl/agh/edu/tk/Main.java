8791a7a55393395a3eadeb046f26e1a29b6740d9
BartoszCh <dsquer888@gmail.com>
2022-04-06 21:26:04 +0200

    minus poprzedni dziala tera

--- src/main/java/pl/agh/edu/tk/Main.java
+++ src/main/java/pl/agh/edu/tk/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner();
-        scanner.Scan("12+2    13-(-12/    5)*9  *   *(-99    +1)");
+        scanner.Scan("12-+2    13-(-12/    5)*9  *   (-99    +1-)");
         System.out.println("\nPrinting list of all tokens:");
         for (String s : scanner.getTokenDescriptionsList()){
             System.out.println(s);
